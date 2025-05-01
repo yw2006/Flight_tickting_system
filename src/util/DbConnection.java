782d70ddc508c78c4532f10ec9b5805a4e279239
yousef_walid <yw012276@gmail.com>
2025-05-01 19:02:41 +0300

    create Database connection class

---
+++ src/util/DbConnection.java
@@ -0,0 +1,33 @@
+package util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import com.mysql.jdbc.Driver;
+public class DbConnection {
+
+    private static DbConnection instance = null;
+    private Connection conn = null;
+
+    private DbConnection() {}
+
+    private void init() throws SQLException {
+        final String DB_URL = "jdbc:mysql://localhost:3309/flights";
+        final String USER = "root";
+        final String PASS = "";
+
+        conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        System.out.println("Connected to database");
+    }
+
+    public Connection getConnection() {
+        return conn;
+    }
+
+    public static Connection getInstance() throws SQLException {
+        if (instance == null || instance.getConnection().isClosed()) {
+            instance = new DbConnection();
+            instance.init();
+        }
+        return instance.getConnection();
+    }
+}
